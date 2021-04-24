225aad7962fd5e1daa5e54252b8715135cafbec1
ViniciusTei <viniciustprates@gmail.com>
2021-04-24 11:41:30 -0300

    :label: typed spotify api return

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import { useEffect } from 'react';
+
+//types
 import { GetStaticProps } from 'next';
+//api
 import { fetchEpisodes } from '../services/api';
 
+//date format
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+
+interface Episode {
+  id: string;
+  title: string
+  thumbnail: string
+  members: string
+  publishedAt: string
+  duration: number
+  description: string
+  url: string
+}
+
 interface Spotify {
-  episodes:  Array<{
-    name: string;
-    id: string;
-    description: string;
-  }>
+  episodes:  Array<Episode>
 }
 
 export default function Home(props: Spotify) {
@@ -27,6 +41,20 @@ export const getStaticProps: GetStaticProps = async () => {
   .then(async (response: any) => {
     const data = await response.json();
     episodes = data.episodes.items
+  })
+
+  episodes = episodes.map((ep)=> {
+    return {
+      id: ep.id,
+      title: ep.name,
+      thumbnail: ep.images[0].url,
+      members: "Nhock e Igor Seco.",
+      publishedAt: format(parseISO(ep.release_date), 'd MMM yy', {locale: ptBR}),
+      duration: Number(ep.duration_ms),
+      description: ep.description,
+      url: ep.audio_preview_url
+
+    }
   })
   return {
     props: {
