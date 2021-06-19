a6f1d87bdeb40881bde51a742be265f9505387df
ViniciusTei <viniciustprates@gmail.com>
2021-06-19 10:09:20 -0300

    add route fetch episode by id

--- src/pages/api/episodes/[id].ts
+++ src/pages/api/episodes/[id].ts
@@ -1,8 +1,22 @@
 import { NextApiResponse, NextApiRequest } from "next";
+import firebase from "../../../services/firebase";
 
 export default async(request: NextApiRequest, response: NextApiResponse) => {
     if(request.method == 'GET') {
         const { id } = request.query
-        return response.status(200).send({id})
+        
+        try {
+            const episodesRef = firebase.collection('episodes')
+            const episode = await episodesRef.doc(id.toString()).get()
+            if (!episode.exists) {
+                return response.status(400).send({message: 'Not found'})
+            }
+            return response.status(200).send({episode: {
+                id: id,
+                ...episode.data()
+            }})
+        } catch (error) {
+            return response.status(500).send({message: 'Internal error', error})
+        }
     }
 }
