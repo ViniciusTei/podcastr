de4ef5c06d9cbf5c366be78e0bcbaf2fb49e77b9
Vincius <viniciustprates@gmail.com>
2021-06-25 07:54:22 -0300

    feat(unsigned): add content and styles
    add unsigned page content and styes

--- src/pages/unsigned.tsx
+++ src/pages/unsigned.tsx
@@ -1,7 +1,27 @@
+import styles from '../styles/unsigned.module.scss';
+
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={styles.pageContainer}>
+            <section className={styles.pageContent}>
+                <img src="/podcast.svg" alt="podcast audience" />
+                <h2>Faça login para começar a ouvir seus podcasts preferidos! 🎵</h2>
+            </section>
+            <section className={styles.pageArticle}>
+                <article>
+                    <h3>O que eh um Podcast?</h3>
+                    <p>A podcast is an episodic series of spoken word digital audio files that a user can download to a personal device for easy listening. 
+                        Streaming applications and podcasting services provide a convenient and integrated way to manage a personal consumption queue across many 
+                        podcast sources and playback devices
+                    </p>
+                </article>
+                <article>
+                    <h3>Como utilizar o podcastr?</h3>
+                    <p>Faca login utilizando sua conta, então eh so começar a adicionar seus podcasts preferidos utilizando o feed rss.</p>
+                </article>
+                
+                
+            </section>
         </div>
     )
 }
