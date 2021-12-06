8b5e6aa04a6ac65b34185c8df044b659ed0eed68
ViniciusTei <viniciustprates@gmail.com>
2021-12-06 13:20:08 -0300

    update to next 12

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     images: {
         domains: ['i.scdn.co', 'd3t3ozftmdmh3i.cloudfront.net', 'lh3.googleusercontent.com', 'desabilitado.com.br', 'firebasestorage.googleapis.com'],
-    }
+    },
+    swcMinify: true,
 }
