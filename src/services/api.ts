35d1f030be5d4d39f0a3b42c5c325d9c9edf624b
ViniciusTei <viniciustprates@gmail.com>
2021-12-07 09:04:27 -0300

    refactor api service to be a class

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,20 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
 
-export const api = axios.create({
-    baseURL: 'http://165.232.126.168/v1'
-})
+export default class Api {
+    public api: AxiosInstance
+
+    constructor(token?: string) {
+        if (token) {
+            this.api = axios.create({
+                baseURL: 'http://165.232.126.168/v1',
+                headers: { Authorization: `Bearer ${token}` }
+            })
+        } else {
+            this.api = axios.create({
+                baseURL: 'http://165.232.126.168/v1',
+            })
+        }
+        
+    }
+}
 
