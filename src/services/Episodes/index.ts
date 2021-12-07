83bf7bc57bb1fce59f7eab830a7e8f3cfdc7c048
ViniciusTei <viniciustprates@gmail.com>
2021-12-07 09:05:54 -0300

    add method to get one episode details

--- src/services/Episodes/index.ts
+++ src/services/Episodes/index.ts
@@ -1,41 +1,41 @@
-import { api } from '../api';
+import Api from '../api';
 
+type Episode = {
+  _id: string;
+  title: string;
+  audioUrl: string;
+  description: string;
+  releaseDate: string;
+  members: string;
+  thumbnail: string;
+  audioLength: number;
+}
 interface EpisodesResponse {
-  data: Array<{
-    _id: string;
-    title: string;
-    audioUrl: string;
-    description: string;
-    releaseDate: string;
-    members: string;
-    thumbnail: string;
-    audioLength: number;
-  }>
+  data: Array<Episode>
   page: number
   pageSize: number
   totalPages: number
 }
 
-type Options = {
-  headers: {
-    Authorization: string;
-  }
-}
-
-export default class EpisodesService {
-  private options: Options;
+export default class EpisodesService extends Api {
 
   constructor(token: string) {
-    this.options = { headers: { Authorization: `Bearer ${token}` } }
+    super(token)
   }
 
   async getMostRecentEpisodes(userId: string): Promise<EpisodesResponse> {
-    const response = await api.get<EpisodesResponse>(`/episodes/${userId}`, this.options)
+    const response = await this.api.get<EpisodesResponse>(`/episodes/${userId}`)
     return response.data
   }
 
   async getEpisodes(userId: string, page: number): Promise<EpisodesResponse> {
-    const response = await api.get(`/episodes/${userId}?page=${page}`, this.options)
+    const response = await this.api.get<EpisodesResponse>(`/episodes/${userId}?page=${page}`)
+    return response.data
+  }
+
+  async getOneEpisodeById(userId: string, episodeId: string): Promise<Episode> {
+    const body = { episodeId }
+    const response = await this.api.get<Episode>(`/episodes/${userId}/detail`, { params: body })
     return response.data
   }
 }
